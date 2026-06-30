f91a90ff274bb5a8fec723584663de31ffbd9db1
zwq <2907551361@qq.com>
2026-06-30 18:45:49 +0800

    文件提交

---
+++ 药物效期小程序/代码文档/源代码/uploadCloudFunction/cloudfunctions/aiAssistant/config.private.js
@@ -0,0 +1,8 @@
+module.exports = {
+  qwenApiKey: "sk-cf30a752f74e4d23bb40c5dd9d849407",
+  zhipuApiKey: "4cecb84d056442a994c562ed86626994.G9ZWmn3uVOiraX4Z",
+  qwenTextModel: "qwen-plus",
+  qwenVisionModel: "qwen-vl-plus",
+  zhipuTextModel: "glm-4.7",
+  asrModel: "paraformer-v2"
+};
